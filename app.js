32061fc3e58f8ea53fef1f1e241c8bfb3327c319
sinclabs <subbalam123@yahoo.co.in>
2016-05-04 18:13:00 +0200

    Web Socket Implementation attempt #7

--- app.js
+++ app.js
@@ -6,18 +6,110 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var fs = require('fs');
-var multer  = require('multer');
-//var connection = require("./local_modules/mysql");
+//var multer  = require('multer');
 var upload = multer({ dest: '/files/' });
-
-var index = require('./routes/index');
-// var dataGrab = require('./routes/dataGrab')
-
+var google = require('googleapis');
+var googleAuth = require('google-auth-library');
 var app = express();
 var WebSocketServer = require("ws").Server;
 var http = require("http");
 var port = process.env.PORT || 5000;
+//var readline = require('readline');
 
+// var SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly'];
+// var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
+//     process.env.USERPROFILE) + '/.credentials/';
+// var TOKEN_PATH = TOKEN_DIR + 'pluto.json';
+
+// fs.readFile('client_secret.json', function processClientSecrets(err, content) {
+//   if (err) {
+//     console.log('Error loading client secret file: ' + err);
+//     return;
+//   }
+//   // Authorize a client with the loaded credentials, then call the
+//   // Drive API.
+//   authorize(JSON.parse(content), listFiles);
+// });
+
+// function authorize(credentials, callback) {
+//   var clientSecret = credentials.installed.client_secret;
+//   var clientId = credentials.installed.client_id;
+//   var redirectUrl = credentials.installed.redirect_uris[0];
+//   var auth = new googleAuth();
+//   var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
+
+//   // Check if we have previously stored a token.
+//   fs.readFile(TOKEN_PATH, function(err, token) {
+//     if (err) {
+//       getNewToken(oauth2Client, callback);
+//     } else {
+//       oauth2Client.credentials = JSON.parse(token);
+//       callback(oauth2Client);
+//     }
+//   });
+// }
+
+// function getNewToken(oauth2Client, callback) {
+//   var authUrl = oauth2Client.generateAuthUrl({
+//     access_type: 'offline',
+//     scope: SCOPES
+//   });
+//   console.log('Authorize this app by visiting this url: ', authUrl);
+//   var rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+//   });
+//   rl.question('Enter the code from that page here: ', function(code) {
+//     rl.close();
+//     oauth2Client.getToken(code, function(err, token) {
+//       if (err) {
+//         console.log('Error while trying to retrieve access token', err);
+//         return;
+//       }
+//       oauth2Client.credentials = token;
+//       storeToken(token);
+//       callback(oauth2Client);
+//     });
+//   });
+// }
+
+// function storeToken(token) {
+//   try {
+//     fs.mkdirSync(TOKEN_DIR);
+//   } catch (err) {
+//     if (err.code != 'EEXIST') {
+//       throw err;
+//     }
+//   }
+//   fs.writeFile(TOKEN_PATH, JSON.stringify(token));
+//   console.log('Token stored to ' + TOKEN_PATH);
+// }
+
+// function listFiles(auth) {
+//   var service = google.drive('v3');
+//   service.files.list({
+//     auth: auth,
+//     pageSize: 10,
+//     fields: "nextPageToken, files(id, name)"
+//   }, function(err, response) {
+//     if (err) {
+//       console.log('The API returned an error: ' + err);
+//       return;
+//     }
+//     var files = response.files;
+//     if (files.length == 0) {
+//       console.log('No files found.');
+//     } else {
+//       console.log('Files:');
+//       for (var i = 0; i < files.length; i++) {
+//         var file = files[i];
+//         console.log('%s (%s)', file.name, file.id);
+//       }
+//     }
+//   });
+// }
+
+var index = require('./routes/index');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
